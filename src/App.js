caf5d1939c905d0d72a731ecea2a965ee7e9c061
Harry Dobbs <hrdobbs@outlook.com>
2022-09-14 13:10:51 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+// https://www.geeksforgeeks.org/how-to-fetch-data-from-an-api-in-reactjs/
+
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ChangeColorButton from './ChangeColorButton';
+
+class App extends React.Component {
+  //Constructor
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: [],
+    }
+  }
+  // componentDidMount is used to
+  // execute the code
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then ((resolution) => resolution.json())
+    .then ((jsonResponse) => {
+      this.setState ({
+        items: jsonResponse,
+      })
+    })
+  }
+
+  render() {
+    const { items } = this.state;
+    return ( 
+      <div className = "App">
+        <h1> Fetch data from an api in react </h1>  {
+            items.map((item) => ( 
+            <ol key = { item.id } >
+                User_Name: { item.username }, 
+                Full_Name: { item.name }, 
+                User_Email: { item.email } 
+                </ol>
+            ))
+        }
+        <ChangeColorButton />
+      </div> 
+    )
+  }
+
+
 }
 
 export default App;
---
+++ src/ChangeColorButton.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './ChangeColorButton.css';
+
+class ChangeColorButton extends React.Component {
+    
+    constructor (props) {
+        super(props)
+    }
+    
+    handleChangeColor() {
+        //let currentColor = 
+        //document.getElementById("buttonId").style.backgroundColor; doesn't work, need value of prop
+        
+        const changeColor = () => {
+            return (
+            document.getElementById("buttonId").style.backgroundColor === 'blue' ?
+            document.getElementById("buttonId").style.backgroundColor = 'blueviolet' :
+            document.getElementById("buttonId").style.backgroundColor = 'blue'
+            );
+        }
+
+        changeColor();
+    }
+
+    render () {
+        return (
+            <button className="colorButton" id="buttonId" onClick={this.handleChangeColor}>
+                CHANGE COLOR BUTTON CLASS RENDERING
+            </button>
+            //<button onClick={handleChangeColor}>Click here to change colour!</button>
+        )
+    }
+}
+
+export default ChangeColorButton;
